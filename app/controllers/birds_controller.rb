71fc488b8b8cc1767b023ffd6f9d6288ba213f7d
Liam <liam.healy1@marist.edu>
2019-11-01 09:10:08 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,10 @@
 # Add code from Readme
+class BirdsController < ApplicationController
+
+    def index
+        @birds = Bird.all
+        # implicitly renders the views/birds/index.html.erb file
+        # because the file name matches this controller#action
+    end
+
+end
